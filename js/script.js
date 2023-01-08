d8f29b5b9e9e5f9a4e4210c653651b34c8ea1cae
Dmytro Andreenko <andreenko2014@gmail.com>
2023-01-08 14:29:30 +0200

    create: site
    Adds:
    1. Header + burger
    2. Main section (beginning)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e){
+	const targetItem = e.target;
+
+	console.log(e.target);
+
+	if (targetItem.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+		
+}
